47d2d758f8cd0dcc07c62da9e33b7937afee43bd
salahelden1818 <salaheldenabdelshafy@gmail.com>
2022-12-12 19:19:51 +0300

    application file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseForm from "./components/ExpenseForm";
+import Alert from "./components/Alert";
+import { v4 as uuidv4 } from "uuid";
+
+// const initialExpenses = [
+//   { id: uuidv4(), charge: "rent", amount: 1600 },
+//   { id: uuidv4(), charge: "car payment", amount: 400 },
+//   { id: uuidv4(), charge: "creadit card bill", amount: 1200 },
+// ];
+
+const initialExpenses = localStorage.getItem("expenses")
+  ? JSON.parse(localStorage.getItem("expenses"))
+  : [];
 
 function App() {
+  // state values
+  // all expense, add expense
+  const [expenses, setExpenses] = useState(initialExpenses);
+  // single expense
+  const [charge, setCharge] = useState("");
+  // single amount
+  const [amount, setAmount] = useState("");
+  // alert
+  const [alert, setAlert] = useState({ show: false });
+  // edit
+  const [edit, setEdit] = useState(false);
+  // edit item
+  const [id, setId] = useState(0);
+
+  // use Effect
+
+  useEffect(() => {
+    console.log("use effect called");
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
+
+  // functionality
+
+  const handleCharge = (e) => {
+    setCharge(e.target.value);
+  };
+
+  const handleAmount = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const handleAlert = ({ type, text }) => {
+    setAlert({ show: true, type, text });
+    setTimeout(() => {
+      setAlert({ show: false });
+    }, 3000);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (charge !== "" && amount > 0) {
+      if (edit) {
+        let tempExpenses = expenses.map((item) => {
+          return item.id === id ? { ...item, charge, amount } : item;
+        });
+        setExpenses(tempExpenses);
+        setEdit(false);
+        handleAlert({ type: "success", text: "Item Edited" });
+      } else {
+        const singleExpense = { id: uuidv4(), charge, amount };
+        setExpenses((oldExpenses) => {
+          return [...oldExpenses, singleExpense];
+        });
+        handleAlert({ type: "success", text: "Item Added" });
+      }
+
+      setCharge("");
+      setAmount("");
+    } else {
+      // handle alert call
+      handleAlert({
+        type: "danger",
+        text: `charge can't be empy value and amount value must be bigger than 0`,
+      });
+    }
+  };
+
+  // clear all items
+  const clearItems = () => {
+    setExpenses([]);
+    handleAlert({ type: "danger", text: "all items deleted" });
+  };
+
+  // handle delete
+  const handleDelete = (id) => {
+    let temExpenses = expenses.filter((item) => item.id !== id);
+    setExpenses(temExpenses);
+    handleAlert({ type: "danger", text: "item deleted" });
+  };
+
+  // handle edit
+  const handleEdit = (id) => {
+    let expense = expenses.find((item) => item.id === id);
+    let { charge, amount } = expense;
+    setCharge(charge);
+    setAmount(amount);
+    setEdit(true);
+    setId(id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleCharge={handleCharge}
+          handleAmount={handleAmount}
+          handleSubmit={handleSubmit}
+          edit={edit}
+        />
+        <ExpenseList
+          expenses={expenses}
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}
+          clearItems={clearItems}
+        />
+      </main>
+      <h1>
+        total spending :{" "}
+        <span className="total">
+          ${" "}
+          {expenses.reduce((acc, curr) => {
+            return (acc += parseInt(curr.amount));
+          }, 0)}
+        </span>
+      </h1>
+    </>
   );
 }
 
